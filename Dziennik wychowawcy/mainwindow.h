424dcfa3a88e4e1141911f61d9f267eceb4ea483
ToSt95 <piotr.krasowski@pollub.edu.pl>
2017-05-16 12:29:11 +0200

    work in progress

---
+++ Dziennik wychowawcy/mainwindow.h
@@ -0,0 +1,31 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlQueryModel>
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QSqlDatabase db;
+    QSqlQueryModel *qu;
+    int islog = 0;
+public:
+   void logowanie();
+private slots:
+    void on_pushButton_clicked();
+};
+
+#endif // MAINWINDOW_H
